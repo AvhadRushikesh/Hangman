d1a3eea28f18229c50ce4e6c6f016bf87a06069a
rushi-smb <rushi@smbios.co.in>
2023-02-25 15:16:37 +0530

    Secting the word to guess

--- Hangman/MainPage.xaml.cs
+++ Hangman/MainPage.xaml.cs
@@ -1,10 +1,52 @@
-﻿namespace Hangman;
+﻿using System.ComponentModel;
+using System.Diagnostics;
 
-public partial class MainPage : ContentPage
+namespace Hangman;
+
+public partial class MainPage : ContentPage, INotifyPropertyChanged
 {
+	#region Fields
+	List<string> words = new List<string>()
+	{
+		"python",
+		"java",
+		"javascrip",
+		"angular",
+		"csharp",
+		"maui",
+		"php",
+		"html",
+		"css",
+		"fsharp",
+		"visualbasic",
+		"powerbi",
+		"signalr",
+		"excel",
+		"paint",
+		"powerpoint",
+		"c",
+		"cpp",
+		"datastructure",
+		"algorithm",
+		"word",
+		"xaml",
+		"mysql",
+		"postgresql",
+		"sqlserver"
+	};
+	string answer = "";
+	#endregion
 	public MainPage()
 	{
 		InitializeComponent();
+		PickWord();
 	}
+	#region Game Engine
+	private void PickWord()
+	{
+		answer = words[new Random().Next(0, words.Count)];
+		Debug.WriteLine(answer);
+	}
+	#endregion
 }
 
